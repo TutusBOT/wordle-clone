1fe989d953893ab7a2032264dd5180d6ac30f7c9
TutusBOT <barte77pl@gmail.com>
2023-02-11 15:36:00 +0100

    fixed file reading

--- pages/api/getDailyWord.ts
+++ pages/api/getDailyWord.ts
@@ -1,29 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import fs from "fs";
+import fsPromises from "fs/promises";
 import axios from "axios";
 
-export default function (req: NextApiRequest, res: NextApiResponse) {
+export default async function getDailyWord(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
 	let dailyWord: string;
 	const date = new Date();
 	const words = axios.get("./api/words");
 
-	const getFile = fs.readFile(
-		`./dailywords/${date.getDate()}-${date.getMonth()}-${date.getFullYear()}.txt`,
-		"utf-8",
-		(e, data) => {
-			// if (data) {
-			// 	dailyWord = data;
-			// 	res.send(data);
-			// 	return;
-			// }
-			// if (e) {
-			// 	console.log(e);
-			// 	return;
-			// }
-
-			res.send("uga");
-		}
+	const fileContents = await fsPromises.readFile(
+		`./dailywords/${date.getDate()}-${date.getMonth()}-${date.getFullYear()}.json`
 	);
 
-	setTimeout(() => console.log(dailyWord), 1000);
+	res.json(JSON.parse(fileContents.toString()));
 }
